aab6d23a22fb18854ce8fe2cb50c05204044ce6e
AlinJoe <alinjoe98@gmail.com>
2023-06-07 18:00:35 +0530

    Initial commit

--- src/container/what gpt/whatgpt.jsx
+++ src/container/what gpt/whatgpt.jsx
@@ -1,11 +1,18 @@
-import React from 'react'
-import './whatgpt.css'
+import React from 'react';
+import './whatgpt.css';
+
 const Whatgpt = () => {
   return (
-    <div>
-      what
+    <div className='fe'>
+      <div className='fea'>featured on</div>
+      <div className='logoda'>
+        <div className='social-links'>
+          <p><a href="https://www.instagram.com" target="_blank" rel="noopener noreferrer">Instagram</a></p>
+          <p><a href="https://www.linkedin.com" target="_blank" rel="noopener noreferrer">LinkedIn</a></p>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Whatgpt
+export default Whatgpt;
